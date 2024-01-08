08b524b54a5acfa03bd77aa4e97ef092acccb70c
MaryamAfify <maryamafify51@gmail.com>
2024-01-08 17:25:17 +0200

    publishable key

--- lib/features/check_out/views/widgets/custom_button_bloc_consumer.dart
+++ lib/features/check_out/views/widgets/custom_button_bloc_consumer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:payment/features/check_out/data/model/payment_intent_input_model.dart';
 import 'package:payment/features/check_out/view_model/cubit/stripe_paymnet_cubit.dart';
 import 'package:payment/features/check_out/view_model/cubit/stripe_paymnet_state.dart';
 import 'package:payment/features/check_out/views/thankyou_view.dart';
@@ -20,14 +21,19 @@ class CustomButtonBlocConsumer extends StatelessWidget {
             return const ThankYouView();
           }));
         }
-        if (state is StripePaymnetFailure){
-          SnackBar snackBar= SnackBar(content: Text(state.errMsg));
+        if (state is StripePaymnetFailure) {
+          SnackBar snackBar = SnackBar(content: Text(state.errMsg));
           ScaffoldMessenger.of(context).showSnackBar(snackBar);
         }
       },
       builder: (context, state) {
         return CustomButton(
-          onTap: () {},
+          onTap: () {
+            PaymentIntentInputModel paymentIntentInputModel =
+                PaymentIntentInputModel(amount: '100', currency: 'USD');
+            BlocProvider.of<StripePaymnetCubit>(context)
+                .makePayment(paymentIntentInputModel: paymentIntentInputModel);
+          },
           text: "Continue",
           isLoading: state is StripePaymnetLoading ? true : false,
         );
